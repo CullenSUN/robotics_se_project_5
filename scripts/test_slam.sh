9ec2c0a58360bd7cea72c7c28f0eddff91155364
Cullen Sun <sunhongshuai@gmail.com>
2022-05-19 00:35:31 +0800

    test

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
-sleep 5
+### xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" 
