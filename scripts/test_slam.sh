4157d1bcc3af4b90f1844f808905f2798d26ec5a
Cullen Sun <sunhongshuai@gmail.com>
2022-05-19 01:05:22 +0800

    update script file

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/../map/NewWorld.world
+
 ### xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" 
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch"
