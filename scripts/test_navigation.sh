b134cb2649ce9ada6abf8dafb2d77f857f45f657
Cullen Sun <sunhongshuai@gmail.com>
2022-05-19 23:48:42 +0800

    update map file location

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/robotics_se_project_5/map/NewWorld.world
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/robotics_se_project_5/map/map.yaml
 
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
