290dab138f7fd7e2a582c8c804e2bb6aedbb84de
Cullen Sun <sunhongshuai@gmail.com>
2022-05-23 22:21:04 +0800

    fix function sequence

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -6,6 +6,20 @@
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 typedef move_base_msgs::MoveBaseGoal MoveBaseGoal;
 
+MoveBaseGoal makeGoalMsg(float x, float y) {
+  MoveBaseGoal goal;
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = x;
+  goal.target_pose.pose.position.y = y;
+  goal.target_pose.pose.orientation.w = 1.0;
+  return goal;
+}
+
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
   ros::init(argc, argv, "pick_objects");
@@ -27,18 +41,4 @@ int main(int argc, char** argv){
     ROS_INFO("The base failed to move forward 1 meter for some reason");
 
   return 0;
-}
-
-MoveBaseGoal makeGoalMsg(float x, float y) {
-  MoveBaseGoal goal;
-
-  // set up the frame parameters
-  goal.target_pose.header.frame_id = "map";
-  goal.target_pose.header.stamp = ros::Time::now();
-
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = x;
-  goal.target_pose.pose.position.y = y;
-  goal.target_pose.pose.orientation.w = 1.0;
-  return goal;
 }
