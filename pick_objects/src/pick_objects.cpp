e6aa9bd1da58238ba3480f823fd8824f8c5e7546
Cullen Sun <sunhongshuai@gmail.com>
2022-05-23 23:45:14 +0800

    further refine function structure

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -6,7 +6,7 @@
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 typedef move_base_msgs::MoveBaseGoal MoveBaseGoal;
 
-MoveBaseGoal makeGoalMsg(float x, float y, float yaw) {
+bool move_to_goal(MoveBaseClient& client, float x, float y, float yaw) {
   MoveBaseGoal goal;
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
@@ -15,7 +15,14 @@ MoveBaseGoal makeGoalMsg(float x, float y, float yaw) {
   goal.target_pose.pose.position.y = y;
   goal.target_pose.pose.orientation.z = yaw;
   goal.target_pose.pose.orientation.w = 1.0;
-  return goal;
+
+  client.sendGoal(goal);
+  client.waitForResult();
+  if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, the base moved to goal");
+  else
+    ROS_INFO("The base failed to move goal for some reason");
+  return client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED;
 }
 
 int main(int argc, char** argv){
@@ -30,23 +37,25 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  MoveBaseGoal pick_up_goal = makeGoalMsg(-4.4, -6.0, -1.57);
-  // -2, 2.5, 1
-  ac.sendGoal(pick_up_goal);
-  ac.waitForResult();
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved to pick_up_goal");
-  else
-    ROS_INFO("The base failed to move pick_up_goal for some reason");
+  move_to_goal(ac, -4.4, -6.0, -1.57);
+  move_to_goal(ac, -1.5, 2.0, 0.0);
 
-  MoveBaseGoal drop_off_goal = makeGoalMsg(-2, 2.5, 0.0);
   // -2, 2.5, 1
-  ac.sendGoal(drop_off_goal);
-  ac.waitForResult();
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved to drop_off_goal");
-  else
-    ROS_INFO("The base failed to move drop_off_goal for some reason");
+  // ac.sendGoal(pick_up_goal);
+  // ac.waitForResult();
+  // if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  //   ROS_INFO("Hooray, the base moved to pick_up_goal");
+  // else
+  //   ROS_INFO("The base failed to move pick_up_goal for some reason");
+
+  // MoveBaseGoal drop_off_goal = makeGoalMsg(-1.5, 2.0, 0.0);
+  // // -2, 2.5, 1
+  // ac.sendGoal(drop_off_goal);
+  // ac.waitForResult();
+  // if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  //   ROS_INFO("Hooray, the base moved to drop_off_goal");
+  // else
+  //   ROS_INFO("The base failed to move drop_off_goal for some reason");
 
 
   return 0;
