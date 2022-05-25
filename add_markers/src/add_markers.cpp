93a106ceb57c7f0a82964586f5c908ba9101b128
Cullen Sun <sunhongshuai@gmail.com>
2022-05-26 00:44:26 +0800

    improve the timed marker logic

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -2,7 +2,7 @@
 #include <visualization_msgs/Marker.h>
 
 
-visualization_msgs::Marker new_marker(float x, float y) {
+visualization_msgs::Marker new_marker(int id, float x, float y) {
     visualization_msgs::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "map";
@@ -11,7 +11,7 @@ visualization_msgs::Marker new_marker(float x, float y) {
     // Set the namespace and id for this marker.  This serves to create a unique ID
     // Any marker sent with the same namespace and id will overwrite the old one
     marker.ns = "home_service_robot_markers";
-    marker.id = 0;
+    marker.id = id;
 
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
     marker.type = visualization_msgs::Marker::CUBE;
@@ -49,15 +49,29 @@ int main( int argc, char** argv )
   ros::init(argc, argv, "add_marker");
   ros::NodeHandle n;
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  while (marker_pub.getNumSubscribers() < 1)
+  {
+    if (!ros::ok())
+    {
+      return 0;
+    }
+    ROS_WARN_ONCE("Please create a subscriber to the marker");
+    sleep(1);
+  }
 
-  visualization_msgs::Marker marker = new_marker(-1.0, 0.0);
-  marker_pub.publish(marker);
+  ROS_INFO("adding marker0"); 
+  visualization_msgs::Marker marker0 = new_marker(0, -1.0, 0.0);
+  marker_pub.publish(marker0);
   sleep(10);
 
-  marker.action = visualization_msgs::Marker::DELETE;
-  marker_pub.publish(marker);
+  ROS_INFO("deleting marker0");
+  marker0.action = visualization_msgs::Marker::DELETE;
+  marker_pub.publish(marker0);
   sleep(10);
 
-  visualization_msgs::Marker marker1 = new_marker(1.0, 0.0);
+  ROS_INFO("adding marker1");
+  visualization_msgs::Marker marker1 = new_marker(1, 1.0, 0.0);
   marker_pub.publish(marker1);
+
+  return 0;
 }
