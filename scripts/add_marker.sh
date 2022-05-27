998006a6b5e49c58ce272c45e3e1d3a48ba3c7f5
Cullen Sun <sunhongshuai@gmail.com>
2022-05-28 02:37:01 +0800

    try dynamic path for the world file

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/robotics_se_project_5/map/NewWorld.world
+# export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/robotics_se_project_5/map/NewWorld.world
 export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/robotics_se_project_5/map/map.yaml
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"$(rospack find add_markers)/../map/NewWorld.world\"" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
